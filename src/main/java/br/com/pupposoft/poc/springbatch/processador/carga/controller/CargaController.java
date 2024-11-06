85b30f7259c928f0c4f26045e41f010bd4688a2c
Renan Furtado <renan.furtado@wedotec.com.br>
2024-11-06 16:59:00 -0300

    Clean code

--- src/main/java/br/com/pupposoft/poc/springbatch/processador/carga/controller/CargaController.java
+++ src/main/java/br/com/pupposoft/poc/springbatch/processador/carga/controller/CargaController.java
@@ -2,21 +2,13 @@ package br.com.pupposoft.poc.springbatch.processador.carga.controller;
 
 import java.io.IOException;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.pupposoft.poc.springbatch.processador.carga.domain.Carga;
